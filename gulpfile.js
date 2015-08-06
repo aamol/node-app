76497e9e3063844611bfb1f70d2bf8a030f3246e
Amol Amol <aamol@sapient.com>
2015-08-06 21:17:05 +0530

    Converting to Handlebar project from jade

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+// include gulp
+var gulp = require('gulp'); 
+
+ 
+// include plug-ins
+var jshint = require('gulp-jshint');
+var changed = require('gulp-changed');
+var imagemin = require('gulp-imagemin');
+
+// JS hint task
+gulp.task('jshint', function() {
+  gulp.src('./*/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+// minify new images
+gulp.task('imagemin', function() {
+  var imgSrc = './public/images/**/*',
+      imgDst = './build/images';
+ 
+  gulp.src(imgSrc)
+    .pipe(changed(imgDst))
+    .pipe(imagemin())
+    .pipe(gulp.dest(imgDst));
+});
