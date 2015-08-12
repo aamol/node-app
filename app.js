b1f3e3d8d2a461d086f99344e6c42cf51db16d78
raghav <rarora9@sapient.com>
2015-08-12 22:55:12 +0530

    Committing changes to print logged-in user welcome message in header

--- app.js
+++ app.js
@@ -90,5 +90,11 @@ hbs.registerHelper('ifCond', function(v1, v2, options) {
   return options.inverse(this);
 });
 
+hbs.registerHelper('elseCond', function(v1, v2, options) {
+  if(v1 !== v2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
 
 module.exports = app;
