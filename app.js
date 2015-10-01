fde2f84681c760a0be1073701833741bfa2579f0
raghav <rarora9@sapient.com>
2015-10-01 11:04:19 +0530

    Adding

--- app.js
+++ app.js
@@ -159,5 +159,10 @@ hbs.registerHelper('lt', function( a, b ){
   return (a < b) ? next.fn(this) : next.inverse(this);
 });
 
+// equal
+hbs.registerHelper('eq', function( a, b ){
+  var next =  arguments[arguments.length-1];
+  return (a === b) ? next.fn(this) : next.inverse(this);
+});
 
 module.exports = app;
